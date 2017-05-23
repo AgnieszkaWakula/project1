1b404a79f32b38cb0607e7cfa26147f7b2f0af54
Agnieszka Wakula <agnieszka.wakula@gmail.comgit config user.emailgit config user.emailgit config user.email agnieszka.wakula@gmail.com>
2017-05-24 00:16:08 +0200

    Add Grunt plugins

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'css/main.css': 'sass/main.scss'
+  			}
+  		}
+  	},
+	watch: {
+    scripts: {
+        files: ['sass/*.scss'],
+        tasks: ['sass'],
+        options: {
+            spawn: false
+        }
+    } 
+},
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	}
+ browserSync: {
+		dev: {
+			bsFiles: {
+				src : ['app/css/*.css','app/*.html']
+			},
+			options: {
+				watchTask: true,
+				server: './app'
+			}
+		}
+	}
+});
+
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  // Default task(s).
+  
+  grunt.registerTask('default', ['sass', 'watch', 'imagemin', 'browser-sync']);
+};
